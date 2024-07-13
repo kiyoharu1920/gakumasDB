41b89bd9c5918c3f224602cc550b48be250405af
kiyoharu1920 <kiyoharu1920@gmail.com>
2024-07-13 14:13:52 +0900

    11111

--- pages/api/getData.ts
+++ pages/api/getData.ts
@@ -2,12 +2,15 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../lib/mongodb";
 
 const getData = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("start1");
   try {
+    console.log("start2");
     const client = await clientPromise;
+    console.log("start3");
     const db = client.db("gakumasDB");
-
+    console.log("start4");
     const data = await db.collection("supportCards").find({}).toArray();
-
+    console.log("start5");
     res.status(200).json(data);
   } catch (e) {
     console.error(e);
@@ -15,4 +18,4 @@ const getData = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export default getData
+export default getData;
