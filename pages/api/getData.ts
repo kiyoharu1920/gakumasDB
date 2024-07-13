59b87047e7f9ea792de45688d5f1650031f06598
kiyoharu1920 <kiyoharu1920@gmail.com>
2024-07-13 14:25:12 +0900

    error追加

--- pages/api/getData.ts
+++ pages/api/getData.ts
@@ -4,8 +4,8 @@ import clientPromise from "../../lib/mongodb";
 const getData = async function (req: NextApiRequest, res: NextApiResponse) {
   try {
     const client = await clientPromise;
-    const db = client.db("your-database-name");
-    const data = await db.collection("your-collection-name").find({}).toArray();
+    const db = client.db("gakumasDB");
+    const data = await db.collection("supportCards").find({}).toArray();
     res.status(200).json(data);
   } catch (error) {
     console.error("Error connecting to the database: ", error);
